c6a01ece93f91bd14d1c92de7be53d5570c54d85
apercam235b <apercam235b@ieszaidinvergeles.org>
2024-05-22 18:54:40 +0200

    alvaro

--- docker-compose/scripts/socket_python.py
+++ docker-compose/scripts/socket_python.py
@@ -1,16 +1,21 @@
 import asyncio
 import websockets
 import re
+import logging
+
+# Configuración del logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 async def listen():
     uri = "ws://172.24.1.200/ws"
     async with websockets.connect(uri) as websocket:
         while True:
             message = await websocket.recv()
-            print(f"Received: {message}")
+            logger.info(f"Received: {message}")
             # Eliminar todos los caracteres que no sean números
             numeric_message = re.sub(r'\D', '', message)
             if numeric_message == "5":
-                print("toma una foto")
+                logger.info("Taking a photo")
 
 asyncio.run(listen())
